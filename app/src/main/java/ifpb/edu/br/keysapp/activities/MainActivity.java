c0dca585744b043b3a1025b409e22b3f24b3e4fc
Mateus de Lima <mateus.lima482@gmail.com>
2017-03-25 15:27:44 -0300

    Primeiro commit, dependencias para retrofit

---
+++ app/src/main/java/ifpb/edu/br/keysapp/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ifpb.edu.br.keysapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ifpb.edu.br.keysapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
